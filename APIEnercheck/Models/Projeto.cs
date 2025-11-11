073d065bae21213423ed178d96e5b478b7db7a7e
Luiz G. <luiz.mwlo76@gmail.com>
2025-11-11 16:34:55 -0300

    Criando Model de Projetos e Criando a controller de usuários

---
+++ APIEnercheck/Models/Projeto.cs
@@ -0,0 +1,15 @@
+﻿namespace APIEnercheck.Models
+{
+    public class Projeto
+    {
+        public Guid ProjetoId { get; set; }
+        public required string Nome { get; set; }
+        public required string Descricao { get; set; }
+        public string UsuarioId { get; set; }
+        public Usuario? Usuario { get; set; }
+        public DateTime dataInicio { get; set; }
+        public int? Progresso { get; set; }
+        public string? Analise { get; set; }
+
+    }
+}
