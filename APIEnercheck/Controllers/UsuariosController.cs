093164940370496b21ccbb1b38e9dbc62d2648fb
Luiz G. <luiz.mwlo76@gmail.com>
2025-11-11 17:05:58 -0300

    Adicionando Endpoints de Usuarios

--- APIEnercheck/Controllers/UsuariosController.cs
+++ APIEnercheck/Controllers/UsuariosController.cs
@@ -1,7 +1,9 @@
-﻿using APIEnercheck.Models;
+﻿using APIEnercheck.Data;
+using APIEnercheck.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,35 +15,68 @@ namespace APIEnercheck.Controllers
     {
         private readonly UserManager<Usuario> _userManager;
         private readonly IAuthorizationService authorizationService;
-        // Colocar DbContext
+        private readonly ApiDbContext _context;
+
+        public UsuariosController(UserManager<Usuario> userManager, IAuthorizationService authorizationService, ApiDbContext context)
+        {
+            _userManager = userManager;
+            this.authorizationService = authorizationService;
+            _context = context;
+        }
+
 
 
         // GET: api/<UsuariosController>
-        [HttpGet]
-        []
-        public IEnumerable<string> Get()
+        [HttpGet("usuarios")]
+        public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
         {
-            return new string[] { "value1", "value2" };
+            return await _context.Usuarios.ToListAsync();
         }
 
         // GET api/<UsuariosController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("usuario/info/{id}")]
+        public async Task<ActionResult<Usuario>> GetUsuario(string id)
         {
-            return "value";
+            var usuario = await _context.Usuarios.FindAsync(id);
+
+            if (usuario == null)
+            {
+                return NotFound("Usuário não encontrado.");
+            }
+            //var identityUser = await _userManager.FindByIdAsync(usuario.Id.ToString());
+
+            return (usuario);
+
         }
 
         // POST api/<UsuariosController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> RegistrarUsuario([FromBody] Usuario model)
         {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var usuario = new Usuario
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                NomeCompleto = model.NomeCompleto,
+                Empresa = model.Empresa,
+                NumeroCrea = model.NumeroCrea,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(usuario);
         }
 
         // PUT api/<UsuariosController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
 
         // DELETE api/<UsuariosController>/5
         [HttpDelete("{id}")]
