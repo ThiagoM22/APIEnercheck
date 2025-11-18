6ae71c42cc94f2aa8101fe56f973016bc7477a91
Thiago Mazzi <thiagodev728@gmail.com>
2025-11-18 15:25:32 -0300

    NMigration

--- APIEnercheck/Migrations/20251118181741_Inicio.Designer.cs
+++ APIEnercheck/Migrations/20251118181741_Inicio.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace APIEnercheck.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    [Migration("20251118175418_Final")]
-    partial class Final
+    [Migration("20251118181741_Inicio")]
+    partial class Inicio
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
