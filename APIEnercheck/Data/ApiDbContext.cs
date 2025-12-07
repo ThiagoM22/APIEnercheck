3c35ca6d6d897bc45ea5e59016661cc481bda66f
Mazzi <thiagodev728@gmail.com>
2025-12-07 17:27:30 -0300

    Erro fatal

--- APIEnercheck/Data/ApiDbContext.cs
+++ APIEnercheck/Data/ApiDbContext.cs
@@ -14,5 +14,14 @@ namespace APIEnercheck.Data
         public DbSet<Projeto> Projeto { get; set; } = default!;
         public DbSet<PagamentoPlano> PagamentoPlanos { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Define explicitamente o tipo de coluna para evitar truncamento de decimais
+            modelBuilder.Entity<PagamentoPlano>()
+                .Property(p => p.ValorPago)
+                .HasColumnType("decimal(10,2)");
         }
+    }
 }
