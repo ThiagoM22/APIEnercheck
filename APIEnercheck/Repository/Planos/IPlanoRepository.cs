6afb06f47a590ecb66c9b33f4339df5c9540fb32
Thiago Mazzi <thiago.mazzi@sti3.com.br>
2026-03-27 00:19:31 -0300

    Adicionando O Repository da Entidade Plano

---
+++ APIEnercheck/Repository/Planos/IPlanoRepository.cs
@@ -0,0 +1,13 @@
+﻿using APIEnercheck.Models;
+
+namespace APIEnercheck.Repository.Planos
+{
+    public interface IPlanoRepository
+    {
+        Task<IEnumerable<Plano>> ListarPlanos();
+        Task<Plano> ObterPlanoPorId(Guid Id);
+        Task<Plano> CriarNovoPlano(Plano plano);
+        Task<Plano> AtualizarPlano(Plano plano);
+        Task<bool> ExcluirPlano(Plano plano);
+    }
+}
---
+++ APIEnercheck/Repository/Planos/PlanoRepository.cs
@@ -0,0 +1,47 @@
+﻿using APIEnercheck.Data;
+using APIEnercheck.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIEnercheck.Repository.Planos
+{
+    public class PlanoRepository
+    {
+        private readonly ApiDbContext _context;
+
+        public PlanoRepository(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Plano>> ListarPlanos()
+        {
+            return await _context.Planos.AsNoTracking().ToListAsync(); ;
+        }
+
+        public async Task<Plano> ObterPlanoPorId(Guid Id)
+        {
+            return await _context.Planos.AsNoTracking().FirstOrDefaultAsync(p => p.Id == Id);
+        }
+
+        public async Task<Plano> CriarNovoPlano(Plano plano)
+        {
+            _context.Planos.Add(plano);
+            await _context.SaveChangesAsync();
+            return plano;
+        }
+
+        public async Task<Plano> AtualizarPlano(Plano plano)
+        {
+            _context.Planos.Update(plano);
+            await _context.SaveChangesAsync();
+            return plano;
+        }
+
+        public async Task<bool> ExcluirPlano(Plano plano)
+        {
+            _context.Planos.Remove(plano);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
